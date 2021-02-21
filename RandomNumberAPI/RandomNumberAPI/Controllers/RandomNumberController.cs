e70e662ab899fb9177ce7670f192e35285b9b2db
yigit2 <yasinyigit2534@gmail.com>
2021-02-21 20:10:08 +0300

    api done

---
+++ RandomNumberAPI/RandomNumberAPI/Controllers/RandomNumberController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomNumberAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomNumberController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetNumbers()
+        {
+            try
+            {
+
+                int length = 6;
+                Random rnd = new Random();
+                ResponseModel model = new ResponseModel();
+                for (int i = 0; i < length; i++)
+                {
+                    model.Numbers.Add(rnd.Next(1, 100).ToString());
+                }
+                model.Status = model.Numbers.Count == 6 ? model.Status = true : model.Status = false;
+                string jsonData = JsonConvert.SerializeObject(model);
+                return Ok(jsonData);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ RandomNumberAPI/RandomNumberAPI/ResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomNumberAPI
+{
+    public class ResponseModel
+    {
+        public  ResponseModel()
+        {
+            Numbers = new List<string>();
+        }
+        public bool Status { get; set; }
+        public List<String> Numbers { get; set; }
+    }
+
+}
